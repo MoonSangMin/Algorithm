34c4a8f31c2495df64327571971c56eb11655a1d
MoonSangMin <49473849+MoonSangMin@users.noreply.github.com>
2020-08-30 15:40:57 +0900

    BOJ 5585

---
+++ BOJ/BOJ 5585/BOJ 5585/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, count = 0;
+	cin >> n;
+
+	n = 1000 - n;
+	while (n != 0) {
+		if (n >= 500) {
+			n -= 500;
+			count++;
+		}
+		else if (n >= 100) {
+			n -= 100;
+			count++;
+		}
+		else if (n >= 50) {
+			n -= 50;
+			count++;
+		}
+		else if (n >= 10) {
+			n -= 10;
+			count++;
+		}
+		else if (n >= 5) {
+			n -= 5;
+			count++;
+		}
+		else {
+			n--;
+			count++;
+		}
+	}
+
+	cout << count;
+}
